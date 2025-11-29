482c507477fecf06b507592807f43d650a02a4d9
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-11-28 22:00:06 -0300

    Add __init__.py to expose analysis and database functions

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .analysis import ranking_alunos_por_curso, plot_alunos_port_turno
+from .database import connect_db
+
+__all__ = ['ranking_alunos_por_curso', 'plot_alunos_port_turno', 'connect_db']
