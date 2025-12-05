ab8ef1d1e6137b4252818b67c8e57e652d00c8b6
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-12-05 09:24:24 -0300

    Remove unused imports and clear __all__ in __init__.py

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .analysis import ranking_alunos_por_curso, plot_alunos_port_turno
-from .database import connect_db
-
-__all__ = ['ranking_alunos_por_curso', 'plot_alunos_port_turno', 'connect_db']
