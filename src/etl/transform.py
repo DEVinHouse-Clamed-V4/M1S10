a9a45e7f8de08c96d7d3b0e4e9215e25ff589f16
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-12-05 09:24:31 -0300

    Enhance data cleaning functions for email, password, and phone number in transform.py

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -24,4 +24,52 @@ def limpar_alunos_csv(df: pd.DataFrame):
 
     df_clean['nome'] = df_clean['nome'].apply(limpar_nome)
 
+    # --------------------
+    # Tratamento email
+    # --------------------
+    def limpar_email(email):
+        if pd.isna(email) or email.strip() == "":
+            return np.nan
+        email = email.strip().lower()
+        email = email.replace('..', '.').replace('@@', '@')
+
+        if '@' not in email or '.' not in email.split('@')[-1]:
+            return np.nan
+        
+        return email
     
+    df_clean['email'] = df_clean['email'].apply(limpar_email)
+
+    # --------------------
+    # Tratamento senha
+    # --------------------
+    def limpar_senha(senha):
+        if pd.isna(senha) or senha.strip() == "":
+            return np.nan
+        return senha.strip()
+
+    df_clean['senha'] = df_clean['senha'].apply(limpar_senha)
+
+    # --------------------
+    # Tratamento telefone
+    # --------------------
+    def limpar_telefone(telefone):
+        if pd.isna(telefone) or telefone.strip() == "":
+            return np.nan
+        telefone = re.sub(r'\D', '', telefone)
+
+        if len(telefone) <= 11:
+            return np.nan
+
+        return telefone
+
+    df_clean['telefone'] = df_clean['telefone'].apply(limpar_telefone)
+
+    # --------------------
+    # Remoção Duplicatas
+    # --------------------
+    df_clean = df_clean.drop_duplicates(subset=['nome', 'email', 'telefone'])
+
+    df_clean = df_clean.reset_index(drop=True)
+
+    return df_clean
